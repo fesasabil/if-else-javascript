4b4e5cd85052e086e910d7a7fec97a262114a47d
fesasabil <kijee212@gmail.com>
2018-09-11 11:39:30 +0700

    game-javascript

---
+++ game.js
@@ -0,0 +1,26 @@
+var nama='';
+var peran='';
+
+if (nama===""  && peran===""){
+	console.log("Nama & peran harus di isi!")
+}
+
+else if(nama && peran===''){
+	console.log("Halo " +nama+ " , pilih peranmu untuk memulai game!")
+}
+
+else if(nama && peran==='ksatria'){
+	console.log("Selamat datang di Dunia Proxitya, " +nama)
+	console.log("Halo Ksatria " +nama+ " , kamu dapat menyerang dengan senjatamu!")
+}
+
+else if(nama && peran==='tabib'){
+	console.log("Selamat datang di Dunia Proxitya, " +nama)
+	console.log("Halo Tabib " +nama+ " , kamu dapat membantu temanmu yang terluka")
+}
+
+else if(nama && peran==='penyihir'){
+	console.log("Selamat datang di Dunia Proxytia, " +nama)
+	console.log("Halo penyihir " +nama+ " , ciptakan keajaiban yang membantu kemenanganmu!")
+    
+}
